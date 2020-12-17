5b4556aa426434671a8a18ffc0a47d1ff9b55737
sanechka <belikalexandra@gmail.com>
2020-12-17 13:03:29 +0300

    init commit

---
+++ src/components/Box.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import * as THREE from 'three';
+
+export default {
+  name: 'Box',
+  methods: {
+    paintBox() {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
